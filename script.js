bfff7aa08e6758cae1d9a597ba37793631c5e888
SandeepM-GitHub <142106485+SandeepM-GitHub@users.noreply.github.com>
2023-09-25 19:18:05 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,11 @@
-function getFormvalue() {
-    //Write your code here
+let Form = document.getElementById('form1');
 
+form.addEventListener('submit', getFormvalue);
+
+function getFormvalue(event) {
+    //Write your code here
+	event.preventDefault();
+	let firstName = document.getElementId('firstName').value;
+	let lastName = document.getElementId('lastName').value;
+	alert('First Name: ${fname}, Last Name: ${lname}');
 }
